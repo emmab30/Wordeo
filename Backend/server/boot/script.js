1ca8118dbaed59510cc48436cc6e117d2a5f04e9
Emmanuel Abuslaiman <eabuslaiman@gmail.com>
2018-05-27 13:01:15 -0300

    Updated backend

--- Backend/server/boot/script.js
+++ Backend/server/boot/script.js
@@ -33,8 +33,7 @@ module.exports = function(server) {
             value: '1.0.0.15'
         });
 
-        server.models.Room.updateAll({}, { isDeleted : true });
-        //server.models.RoomUser.destroyAll({});
+        server.models.Room.destroyAll({});
         //server.models.RoomUser.destroyAll({});
         server.models.Account.update({isOnline: false}, (err, updated) => {
             //Generate random bots if neccesary
