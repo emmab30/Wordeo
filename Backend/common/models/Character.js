9c814417cdc21fb9733cbf2e9ed3e64ce4bb71ec
Emmanuel Abuslaiman <eabuslaiman@gmail.com>
2018-05-28 21:02:25 -0300

    Fix on character buy

--- Backend/common/models/Character.js
+++ Backend/common/models/Character.js
@@ -289,7 +289,7 @@ module.exports = function(Character) {
                     });
                 });
             });
-        });
+        }
     }
 
     function getCharacterByUserId(userId, callback) {
